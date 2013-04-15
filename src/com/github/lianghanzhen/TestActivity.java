1b93e48bd11243aca607bdce8aaf3c220457d0fd
lianghanzhen <handrenliang@gmail.com>
2013-04-15 08:59:44 +0800

    add different position underline

--- src/com/github/lianghanzhen/TestActivity.java
+++ src/com/github/lianghanzhen/TestActivity.java
@@ -1,58 +1,14 @@
 package com.github.lianghanzhen;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.RadioGroup;
 
-public class TestActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
-
-    private UnderlineTextView changeHeightView;
-    private UnderlineTextView changeColorView;
+public class TestActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        changeHeightView = (UnderlineTextView) findViewById(R.id.change_height_text_view);
-        ((RadioGroup) findViewById(R.id.change_height_group)).setOnCheckedChangeListener(this);
-        changeColorView = (UnderlineTextView) findViewById(R.id.change_color_text_view);
-        ((RadioGroup) findViewById(R.id.change_color_group)).setOnCheckedChangeListener(this);
     }
 
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        if (radioGroup.getId() == R.id.change_height_group) {
-            switch (checkedId) {
-                case R.id.radio_button_1dp:
-                    changeHeightView.setUnderlineHeight(getResources().getDimensionPixelSize(R.dimen.dp_1));
-                    break;
-                case R.id.radio_button_3dp:
-                    changeHeightView.setUnderlineHeight(getResources().getDimensionPixelSize(R.dimen.dp_3));
-                    break;
-                case R.id.radio_button_6dp:
-                    changeHeightView.setUnderlineHeight(getResources().getDimensionPixelSize(R.dimen.dp_6));
-                    break;
-                case R.id.radio_button_9dp:
-                    changeHeightView.setUnderlineHeight(getResources().getDimensionPixelSize(R.dimen.dp_9));
-                    break;
-            }
-        } else {
-            switch (checkedId) {
-                case R.id.radio_button_red:
-                    changeColorView.setUnderlineColor(0xffcc0000);
-                    break;
-                case R.id.radio_button_green:
-                    changeColorView.setUnderlineColor(0xff00cc00);
-                    break;
-                case R.id.radio_button_blue:
-                    changeColorView.setUnderlineColor(0xff0000cc);
-                    break;
-                case R.id.radio_button_black:
-                    changeColorView.setUnderlineColor(0xff000000);
-                    break;
-            }
-        }
-    }
 }
--- src/com/github/lianghanzhen/UnderlineTextView.java
+++ src/com/github/lianghanzhen/UnderlineTextView.java
@@ -6,22 +6,25 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 public class UnderlineTextView extends TextView {
+
+    private static final int LEFT = 0;
+    private static final int TOP = 1;
+    private static final int RIGHT = 2;
+    private static final int BOTTOM = 3;
 
     private static final int DEFAULT_UNDERLINE_HEIGHT = 0;
     private static final int DEFAULT_UNDERLINE_COLOR = Color.TRANSPARENT;
 
     private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int mUnderlineHeight;
+    private final int[] mUnderlineHeights = new int[] {DEFAULT_UNDERLINE_HEIGHT, DEFAULT_UNDERLINE_HEIGHT, DEFAULT_UNDERLINE_HEIGHT, DEFAULT_UNDERLINE_HEIGHT};
+    private final int[] mUnderlineColors = new int[] {DEFAULT_UNDERLINE_COLOR, DEFAULT_UNDERLINE_COLOR, DEFAULT_UNDERLINE_COLOR, DEFAULT_UNDERLINE_COLOR};
 
     public UnderlineTextView(Context context) {
         super(context);
-
-        mPaint.setColor(DEFAULT_UNDERLINE_COLOR);
-        mUnderlineHeight = DEFAULT_UNDERLINE_HEIGHT;
-        initUnderlineTextView(context, null);
     }
 
     public UnderlineTextView(Context context, AttributeSet attrs) {
@@ -39,40 +42,68 @@ public class UnderlineTextView extends TextView {
     private void initUnderlineTextView(Context context, AttributeSet attrs) {
         if (attrs != null) {
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.UnderlineTextView);
-            mUnderlineHeight = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_height, DEFAULT_UNDERLINE_HEIGHT);
-            mPaint.setColor(typedArray.getColor(R.styleable.UnderlineTextView_underline_color, DEFAULT_UNDERLINE_HEIGHT));
+            if (typedArray.hasValue(R.styleable.UnderlineTextView_underline_height)) {
+                mUnderlineHeights[LEFT] = mUnderlineHeights[TOP] = mUnderlineHeights[RIGHT] = mUnderlineHeights[BOTTOM] = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_height, DEFAULT_UNDERLINE_HEIGHT);
+            } else {
+                mUnderlineHeights[LEFT] = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_heightLeft, DEFAULT_UNDERLINE_HEIGHT);
+                mUnderlineHeights[TOP] = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_heightTop, DEFAULT_UNDERLINE_HEIGHT);
+                mUnderlineHeights[RIGHT] = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_heightRight, DEFAULT_UNDERLINE_HEIGHT);
+                mUnderlineHeights[BOTTOM] = typedArray.getDimensionPixelSize(R.styleable.UnderlineTextView_underline_heightBottom, DEFAULT_UNDERLINE_HEIGHT);
+            }
+
+            if (typedArray.hasValue(R.styleable.UnderlineTextView_underline_color)) {
+                mUnderlineColors[LEFT] = mUnderlineColors[TOP] = mUnderlineColors[RIGHT] = mUnderlineColors[BOTTOM] = typedArray.getColor(R.styleable.UnderlineTextView_underline_color, DEFAULT_UNDERLINE_COLOR);
+            } else {
+                mUnderlineColors[LEFT] = typedArray.getColor(R.styleable.UnderlineTextView_underline_colorLeft, DEFAULT_UNDERLINE_COLOR);
+                mUnderlineColors[TOP] = typedArray.getColor(R.styleable.UnderlineTextView_underline_colorTop, DEFAULT_UNDERLINE_COLOR);
+                mUnderlineColors[RIGHT] = typedArray.getColor(R.styleable.UnderlineTextView_underline_colorRight, DEFAULT_UNDERLINE_COLOR);
+                mUnderlineColors[BOTTOM] = typedArray.getColor(R.styleable.UnderlineTextView_underline_colorBottom, DEFAULT_UNDERLINE_COLOR);
+            }
+            setPadding(getPaddingLeft() + mUnderlineHeights[LEFT], getPaddingTop() + mUnderlineHeights[TOP], getPaddingRight() + mUnderlineHeights[RIGHT], getPaddingBottom() + mUnderlineHeights[BOTTOM]);
             typedArray.recycle();
         }
-    }
-
-    @Override
-    public void setPadding(int left, int top, int right, int bottom) {
-        super.setPadding(left, top, right, bottom + mUnderlineHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawRect(0, getHeight() - mUnderlineHeight, getWidth(), getHeight(), mPaint);
+        if (mUnderlineHeights[LEFT] > 0) {
+            mPaint.setColor(mUnderlineColors[LEFT]);
+            canvas.drawRect(0, 0, mUnderlineHeights[LEFT], getHeight(), mPaint);
+        }
+        if (mUnderlineHeights[TOP] > 0) {
+            mPaint.setColor(mUnderlineColors[TOP]);
+            canvas.drawRect(0, 0, getWidth(), mUnderlineHeights[TOP], mPaint);
+        }
+        if (mUnderlineHeights[RIGHT] > 0) {
+            mPaint.setColor(mUnderlineColors[RIGHT]);
+            canvas.drawRect(getWidth() - mUnderlineHeights[RIGHT], 0, getWidth(), getHeight(), mPaint);
+        }
+        if (mUnderlineHeights[BOTTOM] > 0) {
+            mPaint.setColor(mUnderlineColors[BOTTOM]);
+            canvas.drawRect(0, getHeight() - mUnderlineHeights[BOTTOM], getWidth(), getHeight(), mPaint);
+        }
     }
 
-    public void setUnderlineHeight(int underlineHeight) {
-        if (underlineHeight < 0) {
-            underlineHeight = 0;
-        }
-        if (mUnderlineHeight != underlineHeight) {
-            int originalPadding = getPaddingBottom() - mUnderlineHeight;
-            mUnderlineHeight = underlineHeight;
-            setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(), originalPadding);
-        }
+    public void setUnderlineHeights(int left, int top, int right, int bottom) {
+        int originalLeft = getPaddingLeft() - mUnderlineHeights[LEFT];
+        mUnderlineHeights[LEFT] = left > 0 ? left : 0;
+        int originalTop = getPaddingLeft() - mUnderlineHeights[TOP];
+        mUnderlineHeights[TOP] = top > 0 ? top : 0;
+        int originalRight = getPaddingLeft() - mUnderlineHeights[RIGHT];
+        mUnderlineHeights[RIGHT] = right > 0 ? right : 0;
+        int originalBottom = getPaddingLeft() - mUnderlineHeights[BOTTOM];
+        mUnderlineHeights[BOTTOM] = bottom > 0 ? bottom : 0;
+        setPadding(originalLeft + mUnderlineHeights[LEFT], originalTop + mUnderlineHeights[TOP], originalRight + mUnderlineHeights[RIGHT], originalBottom + mUnderlineHeights[BOTTOM]);
     }
 
-    public void setUnderlineColor(int underlineColor) {
-        if (mPaint.getColor() != underlineColor) {
-            mPaint.setColor(underlineColor);
-            invalidate();
-        }
+    public void setUnderlineColor(int left, int top, int right, int bottom) {
+        mUnderlineColors[LEFT] = left;
+        mUnderlineColors[TOP] = top;
+        mUnderlineColors[RIGHT] = right;
+        mUnderlineColors[bottom] = bottom;
+        invalidate();
     }
 
 }
